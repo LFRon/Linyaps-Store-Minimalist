2d5039158c74de5ea9e125ff959b3f9ddba7b262
LFRon <ronforever@outlook.com>
2025-10-19 21:17:38 +0800

    初始化关于应用界面

--- lib/pages/main_pages/about_page/about_page.dart
+++ lib/pages/main_pages/about_page/about_page.dart
@@ -10,6 +10,31 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20.0,top: 20,right: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  height: 100,
+                  width: 100,
+                  'assets/images/linyaps_icon.png'
+                ),
+                const SizedBox(width: 60,),
+                Text(
+                  '玲珑应用商店极速版',
+                  style: TextStyle(
+                    fontSize: 50,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
