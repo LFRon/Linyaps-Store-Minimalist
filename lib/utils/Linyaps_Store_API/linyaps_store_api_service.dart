b9d203a8ba1fcf5cfe081db7af7be553339ac7aa
LFRon <ronforever@outlook.com>
2025-11-25 20:36:59 +0800

    fix: 移除玲珑WebEngine的Runtime检查

--- lib/utils/Linyaps_Store_API/linyaps_store_api_service.dart
+++ lib/utils/Linyaps_Store_API/linyaps_store_api_service.dart
@@ -407,7 +407,8 @@ class LinyapsStoreApiService {
         app_info_from_store.id == 'org.deepin.base.flatpak.gnome' ||
         app_info_from_store.id == 'org.deepin.base.wine' ||
         app_info_from_store.id == 'org.deepin.runtime.wine' ||
-        app_info_from_store.id == 'org.deepin.runtime.qt5'
+        app_info_from_store.id == 'org.deepin.runtime.qt5' ||
+        app_info_from_store.id == 'org.deepin.runtime.webengine'
       ) continue;
       // 如果发现有更高版本
       if (
