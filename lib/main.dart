dc5897595931271d3d18138a55b97c8f89704fad
LFRon <ronforever@outlook.com>
2025-12-08 18:13:23 +0800

    fix: 微调代码格式

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: "HarmonyOS Sans SC",
+        fontFamily: 'HarmonyOS Sans SC',
         fontFamilyFallback: [
           'Noto Color Emoji',
         ],
