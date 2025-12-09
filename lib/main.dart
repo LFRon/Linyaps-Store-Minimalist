bc7726e13fb69a09b8a130f1936cec3e20da4c6a
LFRon <ronforever@outlook.com>
2025-12-09 11:49:37 +0800

    bump version: 1.1.4

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ void main() async {
 class MyApp extends StatefulWidget {
 
   // 在这里声明当前应用版本号
-  static String cur_version = '1.1.3';
+  static String cur_version = '1.1.4';
 
   const MyApp({super.key});
   @override
