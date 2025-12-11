a9f2a43e193f150e9a84ead9388c82744faa6487
LFRon <ronforever@outlook.com>
2025-12-11 21:06:34 +0800

    feat: 静态化我的颜色类方法

--- lib/utils/Pages_Utils/my_color/my_color.dart
+++ lib/utils/Pages_Utils/my_color/my_color.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyColor {
   
   // 返回二级颜色
-  Color secondary (BuildContext context) {
+  static Color secondary (BuildContext context) {
     return Theme.of(context).colorScheme.secondary;
   }
 
