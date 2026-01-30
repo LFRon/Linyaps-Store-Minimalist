c24d8a519b390909e13b337dbcbc7fb090b0b462
mozixun <ronforever@outlook.com>
2026-01-30 21:49:06 +0800

    fix: 移除非必要的主题覆写

--- lib/main.dart
+++ lib/main.dart
@@ -92,12 +92,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: yaruLight.colorScheme,
-      ),
-      darkTheme: ThemeData(
-        colorScheme: yaruDark.colorScheme,
-      ),
       themeMode: ThemeMode.system,
       home: YaruTheme(
         data: YaruThemeData(
