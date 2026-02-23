aea0528086bd9e9d812e34bbb9d8b986edb1aba4
LFRon <ronforever@outlook.com>
2026-02-23 12:30:41 +0800

    feat: 下载列表中的应用可以点击直接跳转对应应用详情页面

--- lib/utils/Pages_Utils/downloading_page/downloading_app_list_item.dart
+++ lib/utils/Pages_Utils/downloading_page/downloading_app_list_item.dart
@@ -6,10 +6,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/instance_manager.dart';
+import 'package:linglong_store_flutter/pages/app_info_page/app_info_page.dart';
 import 'package:linglong_store_flutter/utils/Backend_API/Linyaps_Store_API/linyaps_package_info_model/linyaps_package_info.dart';
 import 'package:linglong_store_flutter/utils/GetSystemTheme/syscolor.dart';
 import 'package:linglong_store_flutter/utils/Global_Variables/global_application_state.dart';
 import 'package:linglong_store_flutter/utils/Pages_Utils/generic_buttons/fatal_warning_button.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:yaru/widgets.dart';
 
 class DownloadingAppListItem extends StatelessWidget {
@@ -66,102 +68,124 @@ class DownloadingAppListItem extends StatelessWidget {
                ? Colors.grey.shade800
                : Colors.grey.shade200,
       ),
-      child: Padding(
-        padding: EdgeInsets.only(top:8.0,bottom: 8.0,left: 30.0,right: 35.0),
-        child: Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Row(
-                children: [
-                  CachedNetworkImage(
-                    imageUrl: cur_app_info.Icon ?? '',
-                    placeholder: (context, url) => Center(
-                      child: YaruCircularProgressIndicator(
-                        strokeWidth:2.5,     // 设置加载条宽度
-                      ),
-                    ),
-                    // 如果图片无法加载就使用默认玲珑图标
-                    errorWidget: (context, error, stackTrace) => Center(
-                      child: Image(
-                        image: AssetImage(
-                          'assets/images/linyaps-generic-app.png',
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: InkWell(
+          focusColor: Colors.transparent,
+          hoverColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          onTap: () {
+            Navigator.push(
+              context,
+              PageTransition(
+                type: PageTransitionType.fade,
+                duration: const Duration(milliseconds: 100),
+                reverseDuration: const Duration(milliseconds: 130),
+                child: AppInfoPage(
+                  appId: cur_app_info.id,
+                ),
+              ),
+            );
+          },
+          child: Padding(
+            padding: EdgeInsets.only(top:8.0,bottom: 8.0,left: 30.0,right: 35.0),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    children: [
+                      CachedNetworkImage(
+                        imageUrl: cur_app_info.Icon ?? '',
+                        placeholder: (context, url) => Center(
+                          child: YaruCircularProgressIndicator(
+                            strokeWidth:2.5,     // 设置加载条宽度
+                          ),
                         ),
+                        // 如果图片无法加载就使用默认玲珑图标
+                        errorWidget: (context, error, stackTrace) => Center(
+                          child: Image(
+                            image: AssetImage(
+                              'assets/images/linyaps-generic-app.png',
+                            ),
+                          ),
+                        ),
+                        height: 70,
+                        width: 70,
                       ),
-                    ),
-                    height: 70,
-                    width: 70,
+                      SizedBox(width: 30,),  // 设置应用图标和名称的横向间距
+                      Text(
+                        cur_app_info.name,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 30,),  // 设置应用图标和名称的横向间距
-                  Text(
-                    cur_app_info.name,
+                ),
+          
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    '版本: ${cur_app_info.version}',
                     style: TextStyle(
                       fontSize: 20,
                     ),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
                   ),
-                ],
-              ),
-            ),
-
-            Expanded(
-              flex: 1,
-              child: Text(
-                '版本: ${cur_app_info.version}',
-                style: TextStyle(
-                  fontSize: 20,
                 ),
-              ),
-            ),
-            
-            Expanded(
-              flex: 2,
-              child: cur_app_info.downloadState == DownloadState.downloading
-                ? Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      height: 30,
-                      width: 30,
-                      child: YaruCircularProgressIndicator(
-                        strokeWidth: 3.5,
-                      ),
-                    ),
-                    SizedBox(width: 20,),
-                    Text(
-                      '正在下载',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                )
-                : Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    cur_app_info.downloadState == DownloadState.waiting
-                      ? Row(
-                        children: [
-                          Text(
-                            '正在等待下载',
-                            style: TextStyle(
-                              fontSize: 20,
-                            ),
+                
+                Expanded(
+                  flex: 2,
+                  child: cur_app_info.downloadState == DownloadState.downloading
+                    ? Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                          height: 30,
+                          width: 30,
+                          child: YaruCircularProgressIndicator(
+                            strokeWidth: 3.5,
                           ),
-                          const SizedBox(width: 40,),
-                          SizedBox(
-                            width: 100,
-                            height: 40,
-                            child: cancel_waiting_button,
+                        ),
+                        SizedBox(width: 20,),
+                        Text(
+                          '正在下载',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    )
+                    : Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        cur_app_info.downloadState == DownloadState.waiting
+                          ? Row(
+                            children: [
+                              Text(
+                                '正在等待下载',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                              const SizedBox(width: 40,),
+                              SizedBox(
+                                width: 100,
+                                height: 40,
+                                child: cancel_waiting_button,
+                              )
+                            ],
                           )
-                        ],
-                      )
-                      : SizedBox(),
-                  ],
+                          : SizedBox(),
+                      ],
+                    ),
                 ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
